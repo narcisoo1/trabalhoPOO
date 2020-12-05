7219fed995be7e31b7296f50e56b6c06700e0d28
Euprog <56926468+Euprog@users.noreply.github.com>
2020-12-05 09:04:56 -0300

    Update cadastroProduto.py

--- cadastroProduto.py
+++ cadastroProduto.py
@@ -2,37 +2,40 @@ class CadastroProduto:
 
 	__quantidade_Estoque = 0
 	
-	__slots__ = ['__codigo', '__nome', '__preco']
+	__slots__ = ['__idProduto', '__nomeProduto', '__precoProduto']
 
-	def __init__(self, codigo, nome, preco):
-		self.__codigo = codigo
-		self.__nome = nome
-		self.__preco = preco
+
+	def __init__(self, idProduto, nomeProduto, precoProduto):
+		self.__idProduto = idProduto
+		self.__nomeProduto = nomeProduto
+		self.__precoProduto = precoProduto
 		CadastroProduto.__quantidade_Estoque += 1
 
 	@staticmethod
 	def quantidade_estoque():
 		return CadastroProduto.__quantidade_Estoque
+	@staticmethod
+	def retiraEstoque():
+		CadastroProduto.__quantidade_Estoque -= 1
 
 	@property
-	def codigo(self):
-		return self.__codigo
-
-	@codigo.setter
-	def codigo(self, codigo):
-		self.__codigo = codigo
-
-	@property
-	def nome(self):
-		return self.__nome
-	@nome.setter
-	def nome(self, nome):
-		self.__nome = nome
+	def idProduto(self):
+		return self.__idProduto
+	@idProduto.setter
+	def idProduto(self, idProduto):
+		self.__idProduto = idProduto
 
 	@property
-	def preco(self):
-		return self.__preco
-	@preco.setter
-	def preco(self, preco):
-		self.__preco = preco
+	def nomeProduto(self):
+		return self.__nomeProduto
+	@nomeProduto.setter
+	def nomeProduto(self, nomeProduto):
+		self.__nomeProduto = nomeProduto
+
+	@property
+	def precoProduto(self):
+		return self.__precoProduto
+	@precoProduto.setter
+	def precoProduto(self, precoProduto):
+		self.__precoProduto = precoProduto
 	
