bce28d8bfb3c7daca5b16f74f9c1628760227062
Euprog <56926468+Euprog@users.noreply.github.com>
2021-01-07 00:53:32 -0300

    Update tela_inicial.py

--- tela_inicial.py
+++ tela_inicial.py
@@ -8,7 +8,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_MainWindow(object):
+class Tela_inicial(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(640, 480)
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Tela_inicial()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
