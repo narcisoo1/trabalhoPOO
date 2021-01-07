5a698c16928a33cdf39f164041abf6e8cc38382e
Euprog <56926468+Euprog@users.noreply.github.com>
2021-01-07 00:52:27 -0300

    Update login.py

--- login.py
+++ login.py
@@ -8,7 +8,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_MainWindow(object):
+class Login(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(640, 480)
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Login()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
