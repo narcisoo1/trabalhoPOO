d0cecc9ce7a8cb3ebdf36fafae14af0052f55567
Euprog <56926468+Euprog@users.noreply.github.com>
2021-01-18 16:59:51 -0300

    Update cadatroFuncionario.py

--- cadatroFuncionario.py
+++ cadatroFuncionario.py
@@ -1,10 +1,21 @@
 class OpcoesFuncionario:
     __slots__ = ['_lista_funcionarios']
 
+    """
+    Inicia self._lista_funcionario como lista vazia
+    """
+
     def __init__(self):
         self._lista_funcionarios=[]
 
     def cadastra(self, funcionario):
+        """
+        Cadastra um funcionário
+
+        Retorna True caso consiga cadastrar funcionário e False caso contrário    
+        :param funcionario: Objeto
+        :return: Booleano True ou False
+        """
         existe=self.busca(funcionario.cpf)
         if(existe==None):
             self._lista_funcionarios.append(funcionario)
@@ -13,12 +24,27 @@ class OpcoesFuncionario:
             return False
     
     def busca(self,cpf):
+        """
+        Busca por um funcionário cadastrado
+
+        Retorna o objeto funcionário caso esse funcionário exista na lista self._lista_funcionario, None caso contrário
+        :param cpf: String
+        :return: Objeto ou None
+        """
         for x in self._lista_funcionarios:
             if x.cpf==cpf:
                 return x
         return None
     
     def login(self,usuario,senha):
+        """
+        Verifica se usuário está cadastrado no sistema
+
+        Retorna o objeto funcionário, caso esse existe na lista ou None caso contrário
+        :param usuario: String
+        :param senha: String
+        :return: Objeto ou None
+        """
         for x in self._lista_funcionarios:
             if ((x.login==usuario) and (x.senha==senha)):
                 return x
