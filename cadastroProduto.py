a021290abb6c034e766c3902e5877f577e27b22d
Euprog <56926468+Euprog@users.noreply.github.com>
2021-01-18 16:24:50 -0300

    Update cadastroProduto.py

--- cadastroProduto.py
+++ cadastroProduto.py
@@ -1,11 +1,18 @@
 class CadastroProduto:
     __slots__=['_lista_produtos','_lista_vendas']
-
+    """
+    Inicializa lista de produtos e lista de vendas
+    """
     def __init__(self):
         self._lista_produtos=[]
         self._lista_vendas=[]
     
     def cadastra(self, produto):
+        """
+        retorna True se coseguiu cadastrar produto e False caso contrário
+        :param produto: Objeto
+        :return: Booleano True ou False
+        """
         existe=self.busca(produto.idProduto)
         if(existe==None):
             self._lista_produtos.append(produto)
@@ -19,12 +26,22 @@ class CadastroProduto:
         return False
     
     def busca(self,idProduto):
+        """
+        Retorna id do produto caso encontre, caso contrário retorna None
+        :param idProduto: String
+        :return: String ou None
+        """
         for x in self._lista_produtos:
             if x.idProduto==idProduto:
                 return x
         return None
     
     def venda(self,venda):
+        """
+        Retorna True caso seja possível vender produto, False caso contrário
+        :param venda: Objeto
+        :return: Booleano True ou Falso
+        """
         for x in range(0,len(self._lista_produtos)):
             if venda.nomeProduto == self._lista_produtos[x].nomeProduto:
                 if venda.qtdProduto <= self._lista_produtos[x].qtdProduto:
