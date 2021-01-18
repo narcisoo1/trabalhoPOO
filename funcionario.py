0ce7174b8f0a9a6468d7e537f00809393de75cec
Euprog <56926468+Euprog@users.noreply.github.com>
2021-01-18 17:09:10 -0300

    documentado

--- funcionario.py
+++ funcionario.py
@@ -2,9 +2,22 @@ from pessoa import Pessoa
 
 
 class Funcionario(Pessoa):
+
     __slots__ = ['_nome', '_cpf', '_endereco', '_telefone', '_login', '_senha']
 
+    """
+    Inicializa login e senha
+    """
     def __init__(self, nome=0, cpf=0, endereco=0, telefone=0, senha=0, login=0):
+        """
+        Recebe herança da classe Pessoa os parâmetros descritos:
+        :param nome: String
+        :param cpf: String
+        :param endereco: String
+        :param telefone: String
+        :param senha: String
+        :param login: String
+        """
         super().__init__(nome, cpf, endereco, telefone)
         self._login = login
         self._senha = senha
