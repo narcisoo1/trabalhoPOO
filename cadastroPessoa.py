cdc23500294d0d6502b974e6cd6acaf22d374238
Euprog <56926468+Euprog@users.noreply.github.com>
2021-01-18 16:08:44 -0300

    Update cadastroPessoa.py

--- cadastroPessoa.py
+++ cadastroPessoa.py
@@ -8,11 +8,11 @@ class CadastroPessoa:
         self._lista_pessoas=[]
     
     def cadastra(self, pessoa):
-    	"""
-	Retorna True se conseguiu cadastrar pessoa e False caso contrário.
-	:param pessoa: Objeto
-	:return: Booleano True ou False
-    	"""
+        """
+        Retorna True se conseguiu cadastrar pessoa e False caso contrário.
+        :param pessoa: Objeto
+        :return: Booleano True ou False
+        """
         existe=self.busca(pessoa.cpf)
         if(existe==None):
             self._lista_pessoas.append(pessoa)
@@ -21,21 +21,21 @@ class CadastroPessoa:
             return False
     
     def busca(self,cpf):
-    	"""
-	Retorna uma string se conseguiu encontrar o cpf na self._lista_pessoas, caso contrário retorna None
-	:param cpf: String
-	:return: String ou None
-    	"""
+        """
+        Retorna uma string se conseguiu encontrar o cpf na self._lista_pessoas, caso contrário retorna None
+        :param cpf: String
+        :return: String ou None
+        """
         for x in self._lista_pessoas:
             if x.cpf==cpf:
                 return x
         return None
 
     def vazio(self):
-    	"""
-	Retorna False se a lista self._lista_pessoas estiver vazia, True caso contrário 
-	:return: Booleano True ou Falso
-    	"""
+        """
+        Retorna False se a lista self._lista_pessoas estiver vazia, True caso contrário 
+        :return: Booleano True ou Falso
+        """
         if len(self._lista_pessoas)==0:
             return True
         else:
