36dc40affe8ed168872f6dd40f1d82af91b78cb8
Vitoriacris <viihsrt@gmail.com>
2021-01-23 12:01:53 -0300

    File Sevidor e CLiente

---
+++ cliente.py
@@ -0,0 +1,14 @@
+import socket
+ip = 'localhost'
+port = 8001
+addr = ((ip,port))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(addr)
+
+mensagem = ''
+while(mensagem!= 'sair'):
+    mensagem = input('Digite uma mensagem para enviar ao servidor')
+    client_socket.send(mensagem.encode())
+    print('Mensagem recebida' + client_socket.recv(1024).decode())
+
+client_socket.close()
---
+++ servidor.py
@@ -0,0 +1,22 @@
+import socket
+host = 'localhost'
+port = 8001
+addr = (host, port)
+serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serv_socket.bind(addr)
+serv_socket.listen(10)
+print('Aguarde a conexao...')
+con, cliente = serv_socket.accept()
+print('Conectado')
+print('Aguardando mensagem...')
+
+enviar = ''
+while(enviar!= 'sair'):
+    recebe = con.recv(1024)
+    print('Mensagem recebida' + recebe.decode())
+    enviar = input('Digite uma mensagem para enviar ao Cliente')
+    con.send(enviar.encode())
+
+
+serv_socket.close()
